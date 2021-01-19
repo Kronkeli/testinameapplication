44677236755c19606c641998a92992826908ccc0
Samu Salko <samu.salko@tuni.fi>
2021-01-19 02:27:26 +0200

    Improved visual look and some styling. Implemented sorting by name and total count to React front-end.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,36 +6,134 @@ import List from './List';
 class App extends Component {
   constructor() {
     super();
-    this.state = {isLoaded: false, names : []};
+    this.state = {
+      isLoaded: false, 
+      names : [], 
+      ascendingOrder : false,
+      isFindByName: false,
+      inputName: '',
+      message: "Press a button to list the names..."
+    };
     this.getSortedAlphabetical = this.getSortedAlphabetical.bind(this);
+    this.getSortedAmounts = this.getSortedAmounts.bind(this);
+    this.getCount = this.getCount.bind(this);
+    this.findByName = this.findByName.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
     console.log("appi mountattu");
+    // this.getSortedAmounts();
   }
 
+  // XMLHttprequest GET name list ordered by name
   getSortedAlphabetical(){
-    // create a new XMLHttprequest
+    this.toggleOrder();
     var xhr = new XMLHttpRequest();
     
-    // get a callback when the server responds
     xhr.addEventListener('load', () => {
-      // update the state of the component with the result here
-      console.log(xhr.response);
+      let order = this.state.ascendingOrder ? 'ascending' : 'descending';
       this.setState({
-        names:xhr.response.nameList,
+        names: xhr.response.nameList,
+        message: "The list is ordered by names in " + order + " order:",
+        order: this.toggleOrder,
+        isFindByName: false,
         isLoaded: true
       })
     })
 
-    // open the request with the verb and url
     xhr.open('GET', 'http://localhost:3001/names-by-alphabetical');
     xhr.responseType = 'json';
     xhr.send();
   }
 
+  // XMLHttprequest GET name list ordered by amount
+  getSortedAmounts(){
+    this.toggleOrder();
+    var xhr = new XMLHttpRequest();
+    
+    xhr.addEventListener('load', () => {
+      let order = !this.state.ascendingOrder ? 'ascending' : 'descending';
+      this.setState({
+        names: xhr.response.nameList,
+        message: "The list is ordered by amounts in "+ order + " order:",
+        order: this.toggleOrder,
+        isFindByName: false,
+        isLoaded: true
+      })
+    })
+
+    xhr.open('GET', 'http://localhost:3001/names-by-amount');
+    xhr.responseType = 'json';
+    xhr.send();
+  }
+
+  getCount(){
+    var xhr = new XMLHttpRequest();
+    
+    xhr.addEventListener('load', () => {
+      this.setState({
+        names: [],
+        message: "There are " + xhr.response.count + " names in the list.",
+        isFindByName: false,
+        isLoaded: true
+      })
+    })
+
+    xhr.open('GET', 'http://localhost:3001/names-count');
+    xhr.responseType = 'json';
+    xhr.send();
+  }
+
+  toggleOrder(){
+    this.setState({
+      ascendingOrder: !this.state.ascendingOrder
+    })
+  }
+
+  findByName(){
+    this.setState({
+      isFindByName: true,
+      message: "Write the name you want to find:"
+    })
+    
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render(){
-    if (this.state.names.length === 0) {
+    if (this.state.isFindByName) {
+      return(
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <div className="buttons">
+            <button onClick={this.getSortedAlphabetical}>Sort by names</button>
+            <button onClick={this.getSortedAmounts}>Sort by amounts</button>
+            <button onClick={this.getCount}>Count</button>
+            <button onClick={this.findByName}>Find by name</button>
+          </div>
+          <h3>{this.state.message}</h3>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name:
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+          <input type="submit" value="Submit" />
+          </form>
+        </div>
+      );
+    }
+    else if (this.state.names.length === 0) {
       return (
         <div className="App">
           <header className="App-header">
@@ -43,9 +141,11 @@ class App extends Component {
           </header>
           <div className="buttons">
             <button onClick={this.getSortedAlphabetical}>Sort by names</button>
-            <button>Sort by amounts</button>
+            <button onClick={this.getSortedAmounts}>Sort by amounts</button>
+            <button onClick={this.getCount}>Count</button>
+            <button onClick={this.findByName}>Find by name</button>
           </div>
-          <div>Press a button to list the names...</div>
+          <h3>{this.state.message}</h3>
         </div>
       );
     }
@@ -57,9 +157,12 @@ class App extends Component {
           </header>
           <div className="buttons">
             <button onClick={this.getSortedAlphabetical}>Sort by names</button>
-            <button>Sort by amounts</button>
+            <button onClick={this.getSortedAmounts}>Sort by amounts</button>
+            <button onClick={this.getCount}>Count</button>
+            <button onClick={this.findByName}>Find by name</button>
           </div>
-          <List list={this.state.names} />
+          <h3>{this.state.message}</h3>
+          <List list={this.state.names} order={this.state.ascendingOrder} />
         </div>
       );
     }
--- client/src/List.jsx
+++ client/src/List.jsx
@@ -1,22 +1,36 @@
 import React, {Component} from 'react';
+import './List.css';
 
 class List extends Component {
     constructor(props) {
         super(props);
-        this.state = {items: props.list};
+        this.state = {items: props.list, order: props.order};
         console.log("lista tuntee jutut" + JSON.stringify(props));
     }
 
     componentWillReceiveProps(nextProps) {
-        this.setState({items: nextProps.list});
+        this.setState({items: nextProps.list, order: nextProps.order});
     }
 
     render() {
-        var listItems = this.state.items.map((item, index) =>
+      // myArray.map((val, index, array) => array[array.length - 1 - index]);
+      var listItems;
+      if (!this.state.order) {
+        var reversedItems = this.state.items.map(item => item).reverse();
+        listItems = reversedItems.map((item, index) =>
+        <li key={index}>
+          Name: {item.name} amount: {item.amount}
+        </li>
+        );
+      }
+      else {
+        listItems = this.state.items.map((item, index) =>
         <li key={index}>
             Name: {item.name} amount: {item.amount}
         </li>
         );
+      }
+        
         if (listItems.length === 0 ) {
             listItems = "Unfortunately the name list is empty!";
         };
